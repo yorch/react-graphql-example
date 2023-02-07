216b265f4a079a831f9e3f0afdc17e8af98ca28f
Jorge Barnaby <jorge.barnaby@sentinelone.com>
2023-02-07 10:44:07 -0500

    chore: Cleanup files

--- src/logos/README.md
+++
@@ -1,7 +0,0 @@
-# Logos
-
-These logos were generated using:
-
-```sh
-yarn logos-to-jsx
-```
--- src/logos/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as LogoSentinelOne } from './LogoSentinelOne';
-export { default as LogoSingularityXdr } from './LogoSingularityXdr';
